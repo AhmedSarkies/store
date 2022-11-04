905d4e6a062b18b317ca98f5075312e63898bcee
sarkies <ahmed.sarkes@gmail.com>
2022-11-04 21:51:53 +0200

    New Updates

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Slider from "./components/Slider";
 import "./css/main/main.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Slider />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
