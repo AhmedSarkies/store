cb6883aaaced2baff4dd78d40c2172ea0df2a0f6
sarkies <ahmed.sarkes@gmail.com>
2022-11-04 22:32:00 +0200

    New Updates In Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg bg-light">
-      <div className="container-fluid">
+      <div className="container">
         <Link className="navbar-brand" to={"/"}>
           Logo
         </Link>
@@ -20,7 +20,7 @@ function Navbar() {
         </button>
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-          <li className="nav-item">
+            <li className="nav-item">
               <Link className="nav-link active" aria-current="page" to={"/"}>
                 Home
               </Link>
