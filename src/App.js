57262ce2403aa694ce902a62cf373cc0ecc0c9b5
sarkies <ahmed.sarkes@gmail.com>
2022-11-07 02:18:14 +0200

    New Updates

--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
-import Navbar from "./components/Navbar";
-import Sidebar from "./components/Sidebar";
-import Slider from "./components/Slider";
-import "./css/main/main.css";
+// Import Libraries
+import { Routes, Route, Outlet } from "react-router-dom";
+
+// Import Components
+import Navbar from "./Components/component/Navbar";
+import Sidebar from "./Components/component/Sidebar";
+import Slider from "./Components/component/Slider";
+
+// Import Pages
+import About from "./Components/Pages/About";
+import Home from "./Components/Pages/Home";
+import Products from "./Components/Pages/Products";
+import Categories from "./Components/Pages/Categories";
+
+// Import Style
+import "./Style/css/main.css";
+import AddProduct from "./Components/Pages/AddProduct";
+import ProductDetails from "./Components/Pages/ProductDetails";
+import EditProduct from "./Components/Pages/EditProduct";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      <Slider />
-      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Slider />} />
+      </Routes>
+      <div className="row col-12">
+        <div className="col-3">
+          <Sidebar />
+        </div>
+        <div className="col-9 mt-2 left-side">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/categories" element={<Categories />} />
+            <Route path="/products" element={<Outlet />}>
+              <Route path="" element={<Products />} />
+              <Route path="add" element={<AddProduct />} />
+              <Route path=":productID" element={<EditProduct />} />
+              <Route path=":productID" element={<ProductDetails />} />
+            </Route>
+          </Routes>
+        </div>
+      </div>
     </div>
   );
 }
